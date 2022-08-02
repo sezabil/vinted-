3d834f4b447d65325b293b62cccc92146358da22
bastienlairis <bastien.lairis@gmail.com>
2022-08-02 09:53:27 +0200

    start correction day 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,13 @@ const Home = () => {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
     try {
       const fetchOffers = async () => {
         const response = await axios.get(
-          "https://lereacteur-vinted-api.herokuapp.com/offers"
+          `https://lereacteur-vinted-api.herokuapp.com/offers?limit=5&page=${page}`
         );
         // console.log(response.data);
         setData(response.data);
@@ -23,11 +25,14 @@ const Home = () => {
     } catch (error) {
       console.log(error.message);
     }
-  }, []);
+  }, [page]);
   return isLoading === true ? (
     <div>En cours de chargement</div>
   ) : (
     <div>
+      <h1>{data.count} offres à raison de 5 offre par page</h1>
+      <button onClick={() => setPage(page - 1)}>Page précédente</button>
+      <button onClick={() => setPage(page + 1)}>Page suivante</button>
       {data.offers.map((offer) => {
         // console.log(offer._id);
         return (
